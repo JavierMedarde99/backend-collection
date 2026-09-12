22210b11317a0e89bd9b9f30d67858730dc891da
javimed <javiermedmata@gmail.com>
2026-09-12 08:35:15 +0200

    feat(mongo): unique index on Book and Game externalId

--- src/main/java/com/wikicollection/infrastructure/adapter/out/persistence/BookEntity.java
+++ src/main/java/com/wikicollection/infrastructure/adapter/out/persistence/BookEntity.java
@@ -6,6 +6,7 @@ import com.wikicollection.domain.model.BookState;
 import com.wikicollection.domain.model.BookType;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.AllArgsConstructor;
@@ -27,6 +28,7 @@ public class BookEntity {
     @Id
     private String id;
 
+    @Indexed(unique = true)
     private String externalId;
 
     private String title;
--- src/main/java/com/wikicollection/infrastructure/adapter/out/persistence/GameEntity.java
+++ src/main/java/com/wikicollection/infrastructure/adapter/out/persistence/GameEntity.java
@@ -6,6 +6,7 @@ import com.wikicollection.domain.model.GamePlatform;
 import com.wikicollection.domain.model.GameStatus;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.AllArgsConstructor;
@@ -27,6 +28,7 @@ public class GameEntity {
     @Id
     private String id;
 
+    @Indexed(unique = true)
     private String externalId;
 
     private String title;
